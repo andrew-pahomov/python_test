50721e6e2928fbdd965676962539f30637336014
andrew-pahomov <andrew-pahomov@yandex.ru>
2023-03-08 11:59:42 +0300

    change workflow and tests

--- aqa_unit_pytest.py
+++ aqa_unit_pytest.py
@@ -1,14 +1,13 @@
-import pytest
 import cashback_service
 
 
-class TestPytest:
-    def test_500(self):
-        cashback_servise = cashback_service.CashbackHackService()
-        actual = cashback_servise.remain(500)
-        assert actual == 500
+def test_500():
+    cashback = cashback_service.CashbackHackService()
+    actual = cashback.remain(500)
+    assert actual == 500
 
-    def test_1000(self):
-        cashback_servise = cashback_service.CashbackHackService()
-        actual = cashback_servise.remain(1000)
-        assert actual == 0
+
+def test_1000():
+    cashback = cashback_service.CashbackHackService()
+    actual = cashback.remain(1000)
+    assert actual == 0
